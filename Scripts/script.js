d1c0c95a835d01d90f7ce255d051b199b1e5046b
jackalotUVM <gonzalezc9999@gmail.com>
2021-09-22 10:17:20 -0400

    can now store rows and inputs

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,7 +1,24 @@
 const gameBoard = (() => {
-    let rows = [];
-    return { rows };
+    let rows = ['', '', '',
+                '', '', '',
+                '', '', ''];
+
+    const storeRows = (input, index) => {
+        if(input === 'o' || input === 'x')
+        {
+            console.log("initial: " + rows[index]);
+            rows[index] = input;
+            console.log("final: " + rows[index]);
+        }
+        else
+        {
+            return console.error('cant store something thats not x or o');
+        }
+    } 
+    return { storeRows };
 })();
+
+console.log(gameBoard.storeRows('x', 1))
 const player = (() => {
 
     return;
